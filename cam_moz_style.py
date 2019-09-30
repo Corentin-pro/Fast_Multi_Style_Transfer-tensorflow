0e9414a18659d10eedcce35479abb1fed9d2ef80
corentin <corentin.risselin@cross-compass.com>
2019-09-30 20:15:25 +0900

    Interpolation mosaic

--- cam_moz_style.py
+++ cam_moz_style.py
@@ -11,24 +11,38 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('checkpoint_path', help='Path to checkpoint to load')
     parser.add_argument('--input-size', type=int, default=256, help='Shape of input to use (depends on checkpoint)')
+    parser.add_argument('--inter', nargs='+', type=int, default=256, help='Interpolate between the 4 style given')
     arguments = parser.parse_args()
+
+    style_control = []
+    style_inter = arguments.inter
+    if not style_inter:
+        for style_index in range(16):
+            style_control.append([0.0] * 16)
+            style_control[-1][style_index] = 1
+    else:
+        for col in range(4):
+            for row in range(4):
+                style_index = (col % 4) + (row * 4)
+                style_control.append([0.0] * 16)
+                # top left style
+                style_control[-1][style_inter[0]] = ((3 - row) / 3) * ((3 - col) / 3)
+                # top right style
+                style_control[-1][style_inter[1]] = (row / 3) * ((3 - col) / 3)
+                # bottom left style
+                style_control[-1][style_inter[2]] = ((3 - row) / 3) * (col / 3)
+                # bottom right style
+                style_control[-1][style_inter[3]] = (row / 3) * (col / 3)
+    style_control = np.asarray(style_control, dtype=np.float)
 
     capture = cv2.VideoCapture(-1)
 
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-    import tensorflow as tf
     import tensorflow.compat.v1 as tf1
     from engine_multi import EngineMultiStyle
 
     gpu_options = tf1.GPUOptions(allow_growth=True)
     session_config = tf1.ConfigProto(gpu_options=gpu_options)
-
-    style_control = []
-    for style_index in range(16):
-        style_control.append([0] * 16)
-        style_control[-1][style_index] = 1
-    style_control = np.asarray(style_control, dtype=np.float)
-
     with tf1.Session(config=session_config).as_default() as session:
         input_size = arguments.input_size
         engine = EngineMultiStyle(session, input_size, arguments.checkpoint_path)
@@ -45,8 +59,8 @@ def main():
             for row in range(4):
                 for col in range(4):
                     mosaic[
-                        row * input_size:(row + 1) * input_size,
-                        col * input_size:(col + 1) * input_size] = outputs[
+                        col * input_size:(col + 1) * input_size,
+                        row * input_size:(row + 1) * input_size] = outputs[
                             (4 * col) + row]
 
             # Display the resulting frame
