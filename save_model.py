977b58cf41c6448524706be15a8c228eb85595f7
corentin <corentin.risselin@cross-compass.com>
2019-11-07 17:02:25 +0900

    Save/Load model and customized style count

---
+++ save_model.py
@@ -0,0 +1,30 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('checkpoint_path', help='Path to checkpoint to load')
+    parser.add_argument('--input-size', type=int, default=256, help='Shape of input to use (depends on checkpoint)')
+    parser.add_argument('--output', default='models', help='Directory to output frozen model')
+    arguments = parser.parse_args()
+
+    import tensorflow as tf
+    if tf.__version__.split('.')[0] == '2':
+        import tensorflow.compat.v1 as tf
+        tf.disable_v2_behavior()
+    import tensorflow.compat.v1 as tf1
+    from engine import Engine
+
+    gpu_options = tf1.GPUOptions(allow_growth=True)
+    session_config = tf1.ConfigProto(gpu_options=gpu_options)
+
+    with tf1.Session(config=session_config).as_default() as session:
+        engine = Engine(session, arguments.input_size, arguments.checkpoint_path)
+
+        from tensorflow.python.framework.graph_util import convert_variables_to_constants
+        trained_graph = convert_variables_to_constants(session, session.graph_def, ['Cast_1'])
+        tf.train.write_graph(trained_graph, arguments.output, 'trained_graph.pb', as_text=False)
+        tf.train.write_graph(trained_graph, arguments.output, 'trained_graph.pbtxt', as_text=True)
+
+
+if __name__ == '__main__':
+    main()
